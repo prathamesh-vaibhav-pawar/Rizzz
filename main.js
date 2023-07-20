ea8fd72447ade9b3b304c482d002c94373ac5c23
prathamesh-vaibhav-pawar <107491217+prathamesh-vaibhav-pawar@users.noreply.github.com>
2023-07-20 20:34:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function Next(){
+  document.getElementById("Continue").innerHTML = "Because I'm attracted to youuuuuuu <3"
+}
+function Show(){
+    document.getElementById("Continue").innerHTML = "That's my life without youuu!!"
+
+}
+function Rizz(){
+    document.getElementById("Continue").innerHTML = "Because more I have you the more Im addicted to youuuu!"
+}
+function Rizz_four(){
+    document.getElementById("Continue").innerHTML = "or can I call you mine!"
+}
+function Rizz_Five(){
+    document.getElementById("Continue").innerHTML = "Because you have everything I've been searching for."
+}
+function Rizz_Six(){
+    document.getElementById("Continue").innerHTML = " I keep getting lost in your eyes."
+}
+function Rizz_seven(){
+    document.getElementById("Continue").innerHTML = "Cause my parents always told me to follow my dreams."
+}
